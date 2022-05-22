507d914f593d116e53ddc2c584b237c2b30f6c7c
michelleezhangg <79594869+michelleezhangg@users.noreply.github.com>
2022-05-22 00:10:10 -0700

    added comments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/*
+* Names: Michelle Zhang, Sanil Doshi
+* Student ID: 2380210, 2344493
+* Chapman Email: mizhang@chapman.edu, sdoshi@chapman.edu
+* Course: CPSC 350-01
+* Assignment: Assignment 6 - Building a Database with Binary Search Trees
+*/
 
 #include "DB.h"
 #include "Serialization.h"
