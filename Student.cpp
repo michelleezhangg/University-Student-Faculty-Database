708f9b7c626ecdeba0fdda57df603009b83aee3c
michelleezhangg <79594869+michelleezhangg@users.noreply.github.com>
2022-05-23 14:27:27 -0700

    added comments

--- Student.cpp
+++ Student.cpp
@@ -1,6 +1,16 @@
+/*
+* Names: Michelle Zhang, Sanil Doshi
+* Student IDs: 2380210, 2344493
+* Chapman Emails: mizhang@chapman.edu, sdoshi@chapman.edu
+* Course: CPSC 350-01
+* Assignment: Assignment 6 - Building a Database with Binary Search Trees
+
+* This is the implementation file for the Student Class.
+*/
+
 #include "Student.h"
 
-Student::Student(){
+Student::Student(){ // default constructor
 
     major = "";
     gpa = 0.0;
@@ -8,7 +18,16 @@ Student::Student(){
     studentRecord = new StudentRecords();
 }
 
-Student::Student(string sName, int sID, string sLevel, string sMajor, double sGPA, int sAdvisorID){
+/**
+ * overloaded constructor for the Student class.
+ * @param sName a string representing the name of the student.
+ * @param sID an integer representing the ID of the student.
+ * @param sLevel a string representing the level of the student.
+ * @param sMajor a string representing the major of the student.
+ * @param sGPA a double representing the gpa of the student.
+ * @param sAdvisorID an integer representing the advisor ID of the student.
+ */
+Student::Student(string sName, int sID, string sLevel, string sMajor, double sGPA, int sAdvisorID){ // overloaded constructor
 
     name = sName;
     id = sID;
@@ -19,64 +38,106 @@ Student::Student(string sName, int sID, string sLevel, string sMajor, double sGP
     studentRecord = new StudentRecords(sName, sID, sLevel, sMajor, sGPA, sAdvisorID);
 }
 
-Student::~Student(){}
+Student::~Student(){ // destructor
+    delete studentRecord;
+}
 
-//getters
+// accessors
+/** @return name a string representing the name of the student. */
 string Student::getName(){
 
     return name;
 }
 
+/** @return id an integer representing the ID of the student. */
 int Student::getID(){
 
     return id;
 }
 
+/** @return level a string representing the level of the student. */
 string Student::getLevel(){
 
     return level;
 }
+
+/** @return major a string representing the major of the student. */
 string Student::getMajor(){
 
     return major;
 }
+
+/** @return gpa a double representing the GPA of the student. */
 int Student::getGPA(){
 
     return gpa;
 }
+
+/** @return advisorID an integer representing the advisor ID of the student. */
 int Student::getAdvisorID(){
 
     return advisorID;
 }
 
-//setters
+// mutators
+/**
+ * sets the name of the student.
+ * @param sName a string representing the name of the student.
+ */
 void Student::setName(string sName){
 
     name = sName;
 }
 
+/**
+ * sets the ID of the student.
+ * @param sID an integer representing the ID of the student.
+ */
 void Student::setID(int sID){
 
     id = sID;
 }
 
+/**
+ * sets the level of the student.
+ * @param sLevel a string representing the level of the student.
+ */
 void Student::setLevel(string sLevel){
 
     level = sLevel;
 }
+
+/**
+ * sets the major of the student.
+ * @param sMajor a string representing the major of the student.
+ */
 void Student::setMajor(string sMajor){
 
     major = sMajor;
 }
+
+/**
+ * sets the GPA of the student.
+ * @param sGPA a double representing the GPA of the student.
+ */
 void Student::setGPA(double sGPA){
 
     gpa = sGPA;
 }
+
+/**
+ * sets the advisor ID of the student.
+ * @param sAdvisorID a string representing the advisor ID of the student.
+ */
 void Student::setAdvisorID(int sAdvisorID){
 
     advisorID = sAdvisorID;
 }
 
+/**
+ * gets the student's information in the form of a string.
+ * @return student_info a string representing the student's information.
+ */
 string Student::getStudentInfo() {
     string student_info = "";
 
