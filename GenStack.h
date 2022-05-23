f6519b8f69597e240af9881b6140e5a669cc5077
michelleezhangg <79594869+michelleezhangg@users.noreply.github.com>
2022-05-23 13:06:21 -0700

    added comments

--- GenStack.h
+++ GenStack.h
@@ -1,3 +1,16 @@
+/*
+* Names: Michelle Zhang, Sanil Doshi
+* Student IDs: 2380210, 2344493
+* Chapman Emails: mizhang@chapman.edu, sdoshi@chapman.edu
+* Course: CPSC 350-01
+* Assignment: Assignment 6 - Building a Database with Binary Search Trees
+
+* The template class for the GenStack class.
+* A stack implemented using a Doubly Linked List.
+* This file includes the header and the implementation for the GenStack class.
+* Used for the RollBack class.
+*/
+
 #ifndef GENSTACK_H
 #define GENSTACK_H
 
@@ -16,7 +29,7 @@ class GenStack{
         T pop();
         T peek(); //aka top()
 
-        //aux functions
+        //auxiliary functions
         bool isEmpty();
         int getSize();
 
@@ -27,19 +40,23 @@ class GenStack{
         unsigned int size;
 };
 
+/* implementation */
 template<class T>
-GenStack<T>::GenStack(){
+GenStack<T>::GenStack(){ // default constructor
 
     linkedlist = new DoublyLinkedList<T>();
     size = 0;
 }
 
 template<class T>
-GenStack<T>::~GenStack(){
+GenStack<T>::~GenStack(){ // destructor
     delete linkedlist;
 }
 
-
+/**
+ * adds the element of data data onto the top of the stack.
+ * @param data the element of data we want to push onto the stack.
+ */
 template<class T>
 void GenStack<T>::push(T data){
 
@@ -47,6 +64,10 @@ void GenStack<T>::push(T data){
     size++;
 }
 
+/**
+ * removes the top element of the stack and returns its value.
+ * @return the element of data at the top of the stack.
+ */
 template<class T>
 T GenStack<T>::pop(){
 
@@ -54,6 +75,7 @@ T GenStack<T>::pop(){
     return linkedlist->removeBack();
 }
 
+/** @return the top element's value in the stack. */
 template<class T>
 T GenStack<T>::peek(){
 
@@ -65,12 +87,14 @@ T GenStack<T>::peek(){
     }
 }
 
+/** @return true if the stack is empty and false otherwise. */
 template<class T>
 bool GenStack<T>::isEmpty(){
 
     return size==0;
 }
 
+/** @return size the size of the stack */
 template<class T>
 int GenStack<T>::getSize(){
 
