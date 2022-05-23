516885080ea2f1b5096af13036c6d5cd875245a5
michelleezhangg <79594869+michelleezhangg@users.noreply.github.com>
2022-05-23 13:15:52 -0700

    added comments

--- Person.h
+++ Person.h
@@ -1,3 +1,14 @@
+/*
+* Names: Michelle Zhang, Sanil Doshi
+* Student IDs: 2380210, 2344493
+* Chapman Emails: mizhang@chapman.edu, sdoshi@chapman.edu
+* Course: CPSC 350-01
+* Assignment: Assignment 6 - Building a Database with Binary Search Trees
+
+* This is the header file for the Person class.
+* This is the base class for the Student Class and the Faculty Class.
+*/
+
 #ifndef PERSON_H
 #define PERSON_H
 
@@ -11,12 +22,15 @@ class Person {
 
     public:
 
-        Person();
-        ~Person();
+        Person(); // default constructor
+        ~Person(); // destructor
+    
+        // attributes
         string name;
         int id;
         string level;
 
+        // overloaded operators
         bool operator == (Person &person);
         bool operator != (Person &person);
         bool operator > (Person &person);
