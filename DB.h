30435a1a3293271575b9f7b6937c6773bfda423b
michelleezhangg <79594869+michelleezhangg@users.noreply.github.com>
2022-05-23 12:47:33 -0700

    added comments

--- DB.h
+++ DB.h
@@ -1,3 +1,13 @@
+/*
+* Names: Michelle Zhang, Sanil Doshi
+* Student IDs: 2380210, 2344493
+* Chapman Emails: mizhang@chapman.edu, sdoshi@chapman.edu
+* Course: CPSC 350-01
+* Assignment: Assignment 6 - Building a Database with Binary Search Trees
+
+* This is the header file for the Database Class.
+*/
+
 #ifndef DB_H
 #define DB_H
 
@@ -11,27 +21,29 @@ class DB{
 
     public:
 
-        DB();
-        ~DB();
+        DB(); // default constructor
+        ~DB(); // destructor
 
-        void simulate();
+        void simulate(); // core function
+    
+        // auxiliary functions
         void readFiles();
         int printOptions();
 
-        void printAllStudentInfo(); //prints all student info
-        void printAllFacultyInfo();
-        void printSpecificStudent();
-        void printSpecificFaculty();
-        void printFacultyAdvisor();
-        void printFacultyAdvisees();
-        void addNewStudent();
-        void deleteStudent();
-        void addNewFaculty();
-        void deleteFaculty();
-        void changeStudentAdvisor();
-        void removeAdvisee();
-        void Undo();
-        void Exit();
+        void printAllStudentInfo(); // option 1
+        void printAllFacultyInfo(); // option 2
+        void printSpecificStudent(); // option 3
+        void printSpecificFaculty(); // option 4
+        void printFacultyAdvisor(); // option 5
+        void printFacultyAdvisees(); // option 6
+        void addNewStudent(); // option 7
+        void deleteStudent(); // option 8
+        void addNewFaculty(); // option 9
+        void deleteFaculty(); // option 10
+        void changeStudentAdvisor(); // option 11
+        void removeAdvisee(); // option 12
+        void Undo(); // option 13
+        void Exit(); // option 14
 
 
 
