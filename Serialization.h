b71effb5f095c291f98ad3e502328e1a59703cd7
michelleezhangg <79594869+michelleezhangg@users.noreply.github.com>
2022-05-23 14:15:17 -0700

    add comments

--- Serialization.h
+++ Serialization.h
@@ -1,3 +1,13 @@
+/*
+* Names: Michelle Zhang, Sanil Doshi
+* Student IDs: 2380210, 2344493
+* Chapman Emails: mizhang@chapman.edu, sdoshi@chapman.edu
+* Course: CPSC 350-01
+* Assignment: Assignment 6 - Building a Database with Binary Search Trees
+
+* This is the header file for the Serialization Class.
+*/
+
 #ifndef SERIALIZATION_H
 #define SERIALIZATION_H
 
@@ -22,9 +32,11 @@ class Serialization {
         Serialization(); // default constructor
         ~Serialization(); // destructor
 
+        // write files
         void writeToStudentFile(TreeNode<Student> *student, BST<Student> *masterStudent);
         void writeToFacultyFile(TreeNode<Faculty> *faculty, BST<Faculty> *masterFaculty);
 
+        // read files
         void readToStudentDB(BST<Student> *studentDB);
         void readToFacultyDB(BST<Faculty> *facultyDB);
 
