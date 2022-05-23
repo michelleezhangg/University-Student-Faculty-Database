d4eab674e8dd13ed6aa2159af7171816f28ebfcf
michelleezhangg <79594869+michelleezhangg@users.noreply.github.com>
2022-05-23 13:14:59 -0700

    added comments

--- Person.cpp
+++ Person.cpp
@@ -1,23 +1,54 @@
+/*
+* Names: Michelle Zhang, Sanil Doshi
+* Student IDs: 2380210, 2344493
+* Chapman Emails: mizhang@chapman.edu, sdoshi@chapman.edu
+* Course: CPSC 350-01
+* Assignment: Assignment 6 - Building a Database with Binary Search Trees
+
+* This is the implementation file for the Person class.
+* This is the base class for the Student Class and the Faculty Class.
+*/
+
 #include "Person.h"
 
-Person::Person(){}
-Person::~Person(){}
+Person::Person(){} // default constructor
+Person::~Person(){} // destructor
 
+/**
+ * overloads the operator == for the Person class.
+ * @param &person a Person to compare with operator ==
+ * @return true if the persons' IDs are equal and false otherwise.
+ */
 bool Person::operator == (Person &person){
 
     return id == person.id;
 }
 
+/**
+ * overloads the operator != for the Person class.
+ * @param &person a Person to compare with operator !=
+ * @return true if the persons' IDs are not equal and false otherwise.
+ */
 bool Person::operator != (Person &person){
 
     return id != person.id;
 }
 
+/**
+ * overloads the operator > for the Person class.
+ * @param &person a Person to compare with operator >
+ * @return true if the left person's ID is greater than the right person's and false otherwise.
+ */
 bool Person::operator > (Person &person){
 
     return id > person.id;
 }
 
+/**
+ * overloads the operator < for the Person class.
+ * @param &person a Person to compare with operator <
+ * @return true if the left person's ID is less than the right person's and false otherwise.
+ */
 bool Person::operator < (Person &person){
 
     return id < person.id;
