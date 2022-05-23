2f7212271e6e4b9839a35e21c05a6716978d5b8e
michelleezhangg <79594869+michelleezhangg@users.noreply.github.com>
2022-05-23 12:55:49 -0700

    added comments

--- Faculty.cpp
+++ Faculty.cpp
@@ -1,6 +1,17 @@
+/*
+* Names: Michelle Zhang, Sanil Doshi
+* Student IDs: 2380210, 2344493
+* Chapman Emails: mizhang@chapman.edu, sdoshi@chapman.edu
+* Course: CPSC 350-01
+* Assignment: Assignment 6 - Building a Database with Binary Search Trees
+
+* This is the implementation file for the Faculty Class.
+* This class inherits from Person Class.
+*/
+
 #include "Faculty.h"
 
-Faculty::Faculty(){
+Faculty::Faculty(){ // default constructor
 
     numAdvisees = 0;
     department = "";
@@ -8,7 +19,14 @@ Faculty::Faculty(){
     facultyRecord = new FacultyRecords();
 }
 
-Faculty::Faculty(string fName, int fID, string fLevel, string fDepartment){
+/**
+ * overloaded constructor for the Faculty class.
+ * @param fName a string representing the name of the faculty.
+ * @param fID an integer representing the ID of the faculty.
+ * @param fLevel a string representing the level of the faculty.
+ * @param fDepartment a string representing the department of the faculty.
+ */
+Faculty::Faculty(string fName, int fID, string fLevel, string fDepartment){ // overloaded constructor
 
     name = fName;
     id = fID;
@@ -18,60 +36,102 @@ Faculty::Faculty(string fName, int fID, string fLevel, string fDepartment){
     facultyRecord = new FacultyRecords(fName, fID, fLevel, fDepartment, adviseeIDs);
 }
 
-Faculty::~Faculty(){}
+Faculty::~Faculty(){ // destructor
+    delete facultyRecords;
+    delete adviseeIDs;
+}
 
+// accessors
+/** @return name a string representing the name of the faculty member. */
 string Faculty::getName(){
 
     return name;
 }
 
+/** @return id an integer representing the ID of the faculty member. */
 int Faculty::getID(){
 
     return id;
 }
 
+/** @return level a string representing the level of the faculty member. */
 string Faculty::getLevel(){
 
     return level;
 }
 
+/** @return department a string representing the department of the faculty member. */
 string Faculty::getDepartment(){
 
     return department;
 }
 
+/** @return adviseeIDs a vector storing the advisee IDs of this faculty member. */
 vector<int> Faculty::getAdviseeIDs(){
 
     return *adviseeIDs;
 }
 
+// mutators
+/**
+ * sets the name of the faculty member.
+ * @param fName a string representing the name of the faculty member.
+ */
 void Faculty::setName(string fName){
 
     name = fName;
 }
+
+/**
+ * sets the ID of the faculty member.
+ * @param fID an integer representing the ID of the faculty member.
+ */
 void Faculty::setID(int fID){
 
     id = fID;
 }
+
+/**
+ * sets the level of the faculty member.
+ * @param fLevel a string representing the level of the faculty member.
+ */
 void Faculty::setLevel(string fLevel){
 
     level = fLevel;
 }
+
+/**
+ * sets the department of the faculty member.
+ * @param fDepartment a string representing the department of the faculty member.
+ */
 void Faculty::setDepartment(string fDepartment){
 
     department = fDepartment;
 }
+
+/**
+ * sets the advisee ID vector of the faculty member.
+ * @param aIDs a vector storing the advisee IDs of the faculty member.
+ */
 void Faculty::setAdviseeID(vector<int> *aIDs){
 
     adviseeIDs = aIDs;
 }
 
+/**
+ * adds an advisee ID to the vector of student IDs.
+ * @param id an integer representing the ID to add.
+ */
 void Faculty::addAdvisee(int id){
 
     adviseeIDs->push_back(id);
     numAdvisees++;
 }
 
+/**
+ * removes an advisee ID from the vector of student IDs.
+ * @param id an integer representing the ID to remove.
+ */
 void Faculty::removeAdvisee(int id){
 
     for(int i = 0; i < adviseeIDs->size(); i++) {
@@ -79,10 +139,16 @@ void Faculty::removeAdvisee(int id){
         if(adviseeIDs->at(i) == id){
 
             adviseeIDs->erase(adviseeIDs->begin()+i);
+            numAdvisees--;
         }
     }
 }
 
+/**
+ * returns all of the faculty member's advisees.
+ * handles the case when the faculty member has no advisees.
+ * @return advisees a string representing the faculty member's advisees.
+ */
 string Faculty::getAdviseeID() {
 
     string advisee = "";
@@ -104,6 +170,10 @@ string Faculty::getAdviseeID() {
     return advisee;
 }
 
+/**
+ * gets the faculty member's information in the form of a string.
+ * @return faculty_info a string representing the faculty member's information.
+ */
 string Faculty::getFacultyInfo(){
 
     string faculty_info = "";
@@ -119,6 +189,10 @@ string Faculty::getFacultyInfo(){
     
 }
 
+/**
+ * overloads the operator << for a Faculty object.
+ * @param f a Faculty object we wish to retrieve information from.
+ */
 ostream& operator << (ostream &os, const Faculty &f) {
     os << endl << "FACULTY INFORMATION:" << "\n";
     os << "NAME: " << f.name << "\n";
